329ea63466bc7473a16f34fb6f984eba18fee94b
Mwenje <mwenje24@gmail.com>
2022-07-14 10:25:19 +0300

    added a tags to the Navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 function NavBar() {
   // update the JSX being returned!
   return (
-    <nav></nav>
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
   )
 }
 
